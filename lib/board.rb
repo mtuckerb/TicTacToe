546bdd44887dd709a4624ae52c8a144e907eee0e
Tucker Bradford <tucker@tuckerbradford.com>
2017-09-15 11:08:10 -0400

    added  basic play logic

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,24 @@
 class Board
-  attr_accessor :board
+  attr_accessor :board, :player
 
-  def initialize
+  def initialize(params= {})
+    self.player = params[:player]
     self.board = [[nil,nil,nil],[nil,nil,nil],[nil,nil,nil]]
   end
-  
+
+  def play(x,y)
+      raise InvalidPlayError if board[x][y] != nil
+      board[x][y] = player
+
+  end
+
+end
+
+
+class InvalidPlayError < StandardError
+
+  def initialize(msg="This cell is occupied")
+    super
+  end
+
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,10 +1,23 @@
 require 'board'
 
 describe Board do
+  let(:board) {Board.new(player: "X")}
 
   it "creates a 3x3 array" do
-    board = Board.new
     expect(board.board.size).to eq(3)
   end
 
+  it "takes players letter choice" do
+    expect(board.player).to eq("X")
+  end
+
+  it "allows a player to select a cell" do
+    board.play(1,3)
+    expect(board.board[1][3]).to eq("X")
+  end
+
+  it "doesn't allow player to select an occupied cell" do
+    board.play(1,3)
+    expect {board.play(1,3)}.to raise_error(InvalidPlayError)
+  end
 end
